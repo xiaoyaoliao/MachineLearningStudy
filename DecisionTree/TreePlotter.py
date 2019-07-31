975d609a6836c7069784b60a3f815d5f6446bedf
xiaoyaoliao <1246633193@qq.com>
2019-07-31 14:10:44 +0800

    绘制决策树
    绘制决策树以及决策树相关数据

---
+++ DecisionTree/TreePlotter.py
@@ -0,0 +1,121 @@
+'''
+Created on Oct 14, 2010
+
+@author: Peter Harrington
+'''
+from matplotlib import *
+import matplotlib.pyplot as plt
+
+# 定义文本框和箭头格式，决策节点，叶节点
+decisionNode = dict(boxstyle="sawtooth", fc="0.8")
+leafNode = dict(boxstyle="round4", fc="0.8")
+arrow_args = dict(arrowstyle="<-")
+
+
+def getNumLeafs(myTree):
+    numLeafs = 0
+    # 得到字典中键的第一个值
+    firstSides = list(myTree.keys())
+    firstStr = firstSides[0]
+    secondDict = myTree[firstStr]
+    # print("the secondDict:"+secondDict)
+    # 测试节点的子节点是否为字典，若为字典证明当前节点为判断节点，若不是，则叶节点加一
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            numLeafs += getNumLeafs(secondDict[key])
+        else:
+            numLeafs += 1
+    return numLeafs
+
+
+def getTreeDepth(myTree):
+    maxDepth = 0
+    firstSides = list(myTree.keys())
+    firstStr = firstSides[0]
+    print("the firstStr:" + str(firstStr))
+    secondDict = myTree[firstStr]
+    print("the secondDict:" + str(secondDict))
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':  # test to see if the nodes are dictonaires, if not they are leaf nodes
+            thisDepth = 1 + getTreeDepth(secondDict[key])
+        else:
+            thisDepth = 1
+        if thisDepth > maxDepth: maxDepth = thisDepth
+    return maxDepth
+
+# 绘制箭头的注解
+def plotNode(nodeTxt, centerPt, parentPt, nodeType):
+    createPlot.ax1.annotate(nodeTxt, xy=parentPt, xycoords='axes fraction',
+                            xytext=centerPt, textcoords='axes fraction',
+                            va="center", ha="center", bbox=nodeType, arrowprops=arrow_args)
+
+# 这个是用来绘制线上的标注，简单
+def plotMidText(cntrPt, parentPt, txtString):
+    xMid = (parentPt[0] - cntrPt[0]) / 2.0 + cntrPt[0]
+    yMid = (parentPt[1] - cntrPt[1]) / 2.0 + cntrPt[1]
+    createPlot.ax1.text(xMid, yMid, txtString, va="center", ha="center", rotation=30)
+
+
+def plotTree(myTree, parentPt, nodeTxt):  # if the first key tells you what feat was split on
+    numLeafs = getNumLeafs(myTree)  # this determines the x width of this tree
+    depth = getTreeDepth(myTree)
+    firstSides = list(myTree.keys())
+    firstStr = firstSides[0]
+    # firstStr = myTree.keys()[0]
+    cntrPt = (plotTree.xOff + (1.0 + float(numLeafs)) / 2.0 / plotTree.totalW, plotTree.yOff)
+    plotMidText(cntrPt, parentPt, nodeTxt)
+    plotNode(firstStr, cntrPt, parentPt, decisionNode)
+    secondDict = myTree[firstStr]
+    plotTree.yOff = plotTree.yOff - 1.0 / plotTree.totalD
+    for key in secondDict.keys():
+        if type(secondDict[
+                    key]).__name__ == 'dict':  # test to see if the nodes are dictonaires, if not they are leaf nodes
+            plotTree(secondDict[key], cntrPt, str(key))  # recursion
+        else:  # it's a leaf node print the leaf node
+            plotTree.xOff = plotTree.xOff + 1.0 / plotTree.totalW
+            plotNode(secondDict[key], (plotTree.xOff, plotTree.yOff), cntrPt, leafNode)
+            plotMidText((plotTree.xOff, plotTree.yOff), cntrPt, str(key))
+    plotTree.yOff = plotTree.yOff + 1.0 / plotTree.totalD
+
+
+# if you do get a dictonary you know it's a tree, and the first element will be another dict
+
+def createPlot(inTree):
+    fig = plt.figure(1, facecolor='white')
+    fig.clf()
+    axprops = dict(xticks=[], yticks=[])
+    createPlot.ax1 = plt.subplot(111, frameon=False, **axprops)  # no ticks
+    # createPlot.ax1 = plt.subplot(111, frameon=False) #ticks for demo puropses
+    plotTree.totalW = float(getNumLeafs(inTree))
+    plotTree.totalD = float(getTreeDepth(inTree))
+    plotTree.xOff = -0.5 / plotTree.totalW
+    plotTree.yOff = 1.0;
+    plotTree(inTree, (0.5, 1.0), '')
+    plt.show()
+
+
+# def createPlot():
+#    fig = plt.figure(1, facecolor='white')
+#    fig.clf()
+#    createPlot.ax1 = plt.subplot(111, frameon=False) #ticks for demo puropses
+#    plotNode('a decision node', (0.5, 0.1), (0.1, 0.5), decisionNode)
+#    plotNode('a leaf node', (0.8, 0.1), (0.3, 0.8), leafNode)
+#    plt.show()
+
+def retrieveTree(i):
+    listOfTrees = [{'no surfacing': {0: 'no', 1: {'flippers': {0: 'no', 1: 'yes'}}}},
+                   {'no surfacing': {0: 'no', 1: {'flippers': {0: {'head': {0: 'no', 1: 'yes'}}, 1: 'no'}}}}
+                   ]
+    return listOfTrees[i]
+
+createPlot(retrieveTree(0))
+# leaf1=getNumLeafs(retrieveTree(1))
+# deep1=getTreeDepth(retrieveTree(1))
+# print("the leaf:"+str(leaf1)+"\nthe deep:"+str(deep1))
+# createPlot(thisTree)
+# fig=plt.figure(1,facecolor='white')
+# fig.clf()
+# createPlot.ax1=plt.subplot(111,frameon=False)
+# plotNode('decisionNode',(0.5,0.1),(0.1,0.5),decisionNode)
+# plotNode('leafNode',(0.8,0.1),(0.3,0.8),leafNode)
+# plt.show()
